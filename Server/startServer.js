7aa79bcf1198d4ee3ac8194486e7b332e103330a
Joubert Gruna <joubert.idebrasil@gmail.com>
2023-03-28 12:32:25 -0300

    update router

--- Server/startServer.js
+++ Server/startServer.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res)=>{
+    res.end('Ty!')
+})
+
 const listen = app.listen(3000, (err)=>{
     if(err){
         console.log('Error:', err)
--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const listen = require('./Server/startServer')
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.end('WebSite in development!')
+app.get('/app', (req, res)=>{
+    res.send('WebSite in development!')
 })
 
 listen.listen
